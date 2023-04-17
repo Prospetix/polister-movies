14e827a78be1c41cb77142896524e3d3692c089d
cyplux lukas <prospetix@gmail.com>
2023-04-17 18:47:37 +0100

    second

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,12 +1,19 @@
+import { motion } from 'framer-motion'
 import React from 'react'
 
 function Name() {
-  return (
-    <div className='name'>
-      <h1>Polister movies</h1>
-      <p>best search engine for movies</p>
-    </div>
-  )
+    return (
+        <div className='name'>
+            <motion.h1
+                drag='x'
+                whileDrag={{cursor: "grabbing"}}
+                dragConstraints={{ left: -100, right: 100 }}
+                dragElastic={0.2}
+            >Polister movies
+            </motion.h1>
+            <p>best search engine for movies</p>
+        </div>
+    )
 }
 
 export default Name
